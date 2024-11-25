ad2ce1af647e895473e4c4846017356a5ec47b65
Ayoub Aarab <ayaarab@c1r5p8.1337.ma>
2024-11-25 20:47:56 +0100

    hello

---
+++ script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+arch=$(uname -a)
+
+cpu_physical=$(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l)
+
+cpu_virtual=$(grep "processor" /proc/cpuinfo | wc -l)
+
+memory=$(free -m | awk '/Mem/ {print $3 "/" $2 "MB   ""("$3/$2*100"%)"}')
+
+disk=$(df -h | awk 'NR==4 {print $3 "/" $2 "   ""("$5")"}')
+
+cpu=
+wall "#Architecture: $arch
+	#CPU physical : $cpu_physical
+	#vCPU : $cpu_virtual
+	#Memory Usage: $memory
+	#Disk Usage: 
+	#CPU load: 
+	#Last boot: 
+	#LVM use: 
+	#Connections TCP : 
+	#User log: 
+	#Network: 
+	#Sudo : 
+"
